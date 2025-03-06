e9d8de527f8d31981e6992a29cc059579e9f7724
hdhillon23 <harvird106@deltalearns.ca>
2025-03-06 21:17:09 +0000

    Added psuedocode and if necessary I will change the psuedocode later by removing or adding or changing the wording of the psuedocode.

---
+++ main.py
@@ -0,0 +1,27 @@
+# Enigma Machine with Phone Characters
+
+# Greet the user and ask for their name
+
+# Explain the program
+
+# Ask the user if they want to encrypt or decrypt
+
+# Encrypt function
+
+# Encrypt the message
+
+# Print the encrypted message
+
+# Decrypt function
+
+# Decrypt the message
+
+# Print the encrypted message
+
+# Ask the user if they want to play again [y/n]
+
+# If "y" restart the program
+
+# If "n" stop/end the program
+
+# Say goodbye
