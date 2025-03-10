c60ab329c996d237deb943742357cdec820d4a59
hdhillon23 <harvird106@deltalearns.ca>
2025-03-10 16:40:28 +0000

    I made it where if the user chooses 'e' for encrypt it asks what message they want to encrypt and it handles numbers where you can only type in letters and spaces only. I made it fool/child proof for encrypt the message. I also moved the encrypt and decrypt functions psuedocode up to the top.

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-# Enigma Machine with Phone Characters
+# Start Enigma Machine with Phone Characters
+
+# Encrypt function
+
+# Decrypt function
 
 # Greet the user and ask for their name and explain the program
 user_name = input("What is your name before we get this program started?")
 print(f"Greetings {user_name}! This is an engima machine program\
  that converts phone number characters into letters!")
 
-# Ask the user if they want to encrypt or decrypt
-choice = input(f"{user_name} would you like to encrypt or decrypt a message")
+while True:
+    # Ask the user if they want to encrypt or decrypt
+    choice = input(f"{user_name} would you like to encrypt or decrypt a message? (e/d): ").strip().lower()
+    while choice not in ['e','d']:
+        choice = input("Invalid choice. Please choose 'e' for encrypt or 'd' for decrypt: ").strip().lower()
 
-# Encrypt function
+    # Encrypt the message
+    if choice == 'e':
+        message = input("Enter the message you want to encrypt (Type letters and spaces only): ").strip()
+        while not all(char.isalpha() or char.isspace() for char in message):
+            message = input("Invalid input. Please enter a message containing letters and spaces only: ").strip()
+            encrypted_mssage = encrypt_message(message)
+            print(f"Encrypted message: {encrypt_message}")
 
-# Encrypt the message
+    # Print the encrypted message
 
-# Print the encrypted message
+    # Decrypt the message
 
-# Decrypt function
+    # Print the encrypted message
 
-# Decrypt the message
+    # Ask the user if they want to play again [y/n]
 
-# Print the encrypted message
+    # If "y" restart the program
 
-# Ask the user if they want to play again [y/n]
+    # If "n" stop/end the program
 
-# If "y" restart the program
-
-# If "n" stop/end the program
-
-# Say goodbye
+    # Say goodbye
