e59842d0ee723669340115e3f644ab4b36d863ae
hdhillon23 <harvird106@deltalearns.ca>
2025-03-11 18:30:01 +0000

    I changed the line of psuedocode for the function to encrypt a messgae and I made it more detailed to explain the function and I finished thta funnction.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # Start Enigma Machine with Phone Characters
 
-# Encrypt function
+# Function to encrypt a message using phone characters
+def encrypt_message(message):
+    phone_dict = {
+     'A': '2', 'B': '22', 'C': '222',
+     'D': '3', 'E': '33', 'F': '333',
+     'G': '4', 'H': '44', 'I': '444',
+     'J': '5', 'K': '55', 'L': '555',
+     'M': '6', 'N': '66', 'O': '666',
+     'P': '7', 'Q': '77', 'R': '777', 'S': '7777',
+     'T': '8', 'U': '88', 'V': '888',
+     'W': '9', 'X': '99', 'Y': '999', 'Z': '9999',
+
+    }
 
 # Decrypt function
 
