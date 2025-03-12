0d1acf5f2c63338cb0bb1196348bcf0be686a3bd
hdhillon23 <harvird106@deltalearns.ca>
2025-03-12 16:53:59 +0000

    I changed the line of psuedocode for the function to decrypt a messgae and I made it more detailed to explain the function and I finished that funnction.

--- main.py
+++ main.py
@@ -14,7 +14,20 @@ def encrypt_message(message):
 
     }
 
-# Decrypt function
+# Function to decrypt a message back to text
+def decrypt_message(encrypted_text):
+    reverse_dict = {
+     '2': 'A', '22': 'B', '222': 'C',
+     '3': 'D', '33': 'E', '333': 'F',
+     '4': 'G', '44': 'H', '444': 'I',
+     '5': 'J', '55': 'K', '555': 'L',
+     '6': 'M', '66': 'M', '666': 'O',
+     '7': 'P', '77': 'Q', '777': 'R', '7777': 'S',
+     '8': 'T', '88': 'U', '888': 'V',
+     '9': 'W', '99': 'X', '999': 'Y', '9999': 'Z',
+
+    }
+
 
 # Greet the user and ask for their name and explain the program
 user_name = input("What is your name before we get this program started?")
